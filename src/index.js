5f33ff24f66780b464624393c8c780deeb0203bc
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-10-11 07:23:51 +0300

    add components, props, propsTypes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const element = React.createElement("div", { a: 5, b: 10, children: "hello world!" });
-console.log(element);
+// const element = React.createElement("div", { a: 5, b: 10, children: "Hello World!" });
+// console.log(element);
+
+// const element1 = <span>Hello</span>;
+// React.createElement("span", { children: "World" });
+// const element2 = <span>World!</span>;
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// const jsxElement = (
+//   <div>
+//     {element1}
+//     {element2}
+//   </div>
+// );
+// console.log(jsxElement);
+
+// root.render(jsxElement);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(element);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // ////////////////////////////////////////////////
 // import "./index.css";
@@ -15,6 +36,6 @@ root.render(element);
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
 //   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>
 // );
