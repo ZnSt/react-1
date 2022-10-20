52587cda001cf0bf91f0a5fe3154fbb9478f6df9
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-10-20 06:09:59 +0300

    add form

--- src/App.js
+++ src/App.js
@@ -21,7 +21,21 @@ export class App extends Component {
       { id: "id-2", text: "Разобраться с реакт роутер", completed: false },
       { id: "id-3", text: "Пережить редакс", completed: false },
     ],
+
+    name: "",
+    tag: "",
   };
+
+  handleChange = (event) => {};
+
+  // handleNameChange = (event) => {
+
+  //   this.setState({ name: event.currentTarget.value });
+  // };
+
+  // handleTagChange = (event) => {
+  //   this.setState({ tag: event.currentTarget.value });
+  // };
 
   deleteTodo = (todoId) => {
     this.setState((prevState) => ({
@@ -35,6 +49,16 @@ export class App extends Component {
     const compeletedTodos = todos.reduce((acc, todo) => (todo.completed ? acc + 1 : acc), 0);
     return (
       <>
+        <form>
+          <label htmlFor="">
+            Name{" "}
+            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+          </label>
+          <label htmlFor="">
+            Nickname{" "}
+            <input type="text" name="tag" value={this.state.tag} onChange={this.handleChange} />
+          </label>
+        </form>
         {/* return <Counter initialValue={0} />; */}
 
         {/* return <Dropdown />; */}
