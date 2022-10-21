618e3eb7317c84ae942290f004166c6e29523559
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-10-21 17:56:32 +0300

    remove file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import "./App.css";
-import paintings from "./painting.json";
-import PaintingList from "./components/PaintingList";
-import Sections from "./components/Sections";
 
 export default function App() {
-  return (
-    <div>
-      <Sections title="Топ недели">
-        <PaintingList items={paintings} />
-      </Sections>
-    </div>
-  );
+  return <div></div>;
 }
