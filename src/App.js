2a36cf7cf335cea9f4def367a0dce249e64a0e5e
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-11-07 19:43:29 +0200

    remove another file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import "./App.css";
-import paintings from "./painting.json";
-import PaintingList from "./components/PaintingList";
-import Sections from "./components/Sections";
+import { Component } from "react";
 
-export default function App() {
-  return (
-    <div>
-      <Sections title="Топ недели">
-        <PaintingList items={paintings} />
-      </Sections>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    page: 1,
+    query: "",
+    items: [],
+  };
+
+  loadMore = () => {
+    this.setState((prevState) => ({
+      page: prevState.page + 1,
+    }));
+  };
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.page !== this.state.page || prevState.query !== this.state.query) {
+      console.log("fetch.data");
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ page: 1, query: event.target.elements.query.value, items: [] });
+    event.target.reset();
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="query" />
+          <button type="submit">Search</button>
+        </form>
+
+        <button onClick={this.loadMore}>Load More</button>
+      </div>
+    );
+  }
 }
